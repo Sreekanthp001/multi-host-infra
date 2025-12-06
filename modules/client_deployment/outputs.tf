9441773c6caa1ce819b5563b50acfa46463ad92d
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-12-06 17:57:43 +0530

    alb ecs creation

--- modules/client_deployment/outputs.tf
+++ modules/client_deployment/outputs.tf
@@ -2,15 +2,15 @@
 
 output "target_group_arn" {
   description = "The ARN of the ECS Target Group created for this client."
-  value       = aws_lb_target_group.client_tg["my_test_client"].arn
+  value       = aws_lb_target_group.client_tg["sree84s.site"].arn
 }
 
 output "listener_rule_arn" {
   description = "The ARN of the ALB Listener Rule created for this client's domain."
-  value       = aws_lb_listener_rule.host_rule["my_test_client"].arn
+  value       = aws_lb_listener_rule.host_rule["sree84s.site"].arn
 }
 
 output "ecs_service_name" {
   description = "The name of the ECS service deployed for this client."
-  value       = aws_ecs_service.client_service["my_test_client"].name
+  value       = aws_ecs_service.client_service["sree84s.site"].name
 }

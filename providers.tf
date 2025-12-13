312b5628794a70323591f2c81a43c434ae5c50dc
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-12-13 14:20:54 +0530

    Configured S3 backend in providers.tf to fix state errors

--- providers.tf
+++ providers.tf
@@ -5,7 +5,18 @@ terraform {
       version = "~> 5.0" # Use a version compatible with your project
     }
   }
+
+  # **** కొత్తగా చేర్చబడిన S3 Backend కాన్ఫిగరేషన్ ****
+  backend "s3" {
+    # ఇక్కడ మీరు మీ అసలు S3 బకెట్ పేరును ఉంచాలి!
+    bucket  = "మీ-S3-బకెట్-పేరు" 
+    key     = "multi-host-infra/terraform.tfstate"
+    region  = "us-east-1" # Backend బకెట్ ఉన్న రీజియన్
+    encrypt = true
+  }
+  # *******************************************************
 }
+
 # Default Provider (Uses the variable from variables.tf)
 # providers.tf (In your ROOT directory)
 

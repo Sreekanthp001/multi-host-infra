8b9752661394b650fd55c91023b82d060883eb76
sreekanthp <varshisree2@gmail.com>
2025-12-18 10:48:46 +0530

    changes in ses

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "alb_dns_name" {
   value = module.alb.alb_dns_name
 }
+
+output "client_smtp_secrets" {
+  description = "Individual SMTP credentials for each client"
+  value       = module.ses_config.client_smtp_secrets
+  sensitive   = true
+}

1853f5e206023d13b0febac9d6ae4c102fad1908
sreekanthp <varshisree2@gmail.com>
2025-12-19 10:43:12 +0530

    rule set changed

--- modules/ses_config/variables.tf
+++ modules/ses_config/variables.tf
@@ -1,26 +1,23 @@
-variable "project_name" {
-  description = "The prefix to use for all SES related resources."
-  type        = string
+variable "project_name" { type = string }
+variable "aws_region"   { type = string }
+variable "forwarding_email" { type = string }
+
+# Root lo pass chesthunnav kabatti idhi kachithanga undali
+variable "all_client_domains" {
+  type    = list(string)
+  default = []
 }
 
-variable "client_domains" {
-  description = "A map of client keys (e.g., 'sree84s') to their full domain names (e.g., 'sree84s.site')."
-  type        = map(string)
-}
-
-variable "aws_region" {
-  description = "The AWS region where SES is configured (e.g., us-east-1)."
-  type        = string
-}
-
-variable "forwarding_email" {
-  description = "Email address to forward incoming SES mail to (e.g., your personal Gmail ID)."
-  type        = string
-}
+# Idhi for_each logic kosam
 variable "client_configs_map" {
   type = map(object({
     domain_name    = string
     hosting_type   = string
     email_accounts = list(string)
   }))
+}
+
+# Root main.tf lo pampistunnav kabatti idhi kuda pettu
+variable "client_domains" {
+  type = map(string)
 }

e1450a21bd326578f511759f3be8e3079f9003ea
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2026-02-08 10:57:20 +0530

    outputs changing

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -1,11 +1,14 @@
 output "cluster_id" {
+  # Resource name 'main' match avvali
   value = aws_ecs_cluster.main.id
 }
 
 output "task_definition_arn" {
-  value = aws_ecs_task_definition.app.arn
+  # Resource name 'main' match avvali (ne daggara 'app' ledu)
+  value = aws_ecs_task_definition.main.arn
 }
 
 output "ecs_service_sg_id" {
-  value = aws_security_group.ecs_tasks.id
+  # Resource name 'ecs_tasks_sg' match avvali
+  value = aws_security_group.ecs_tasks_sg.id
 }

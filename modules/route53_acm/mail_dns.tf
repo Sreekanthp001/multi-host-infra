6c2c32ab47ec664a90c01e7e40b49f5b0df6f0f0
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2026-02-08 19:44:38 +0530

    adding emails

--- modules/route53_acm/mail_dns.tf
+++ modules/route53_acm/mail_dns.tf
@@ -1,6 +1,10 @@
 # 1. Primary A Record for the Mail Server (mx.primarydomain.com)
 resource "aws_route53_record" "mail_server_a_record" {
-  count   = var.main_domain != "" && var.mail_server_ip != "" ? 1 : 0
+  # count value must be known at plan time. var.main_domain is passed as a string/variable,
+  # but mail_server_ip is a resource attribute (EIP) known only after apply.
+  # Removing the IP check to fix the "Invalid count argument" error.
+  count   = var.main_domain != "" ? 1 : 0
+  
   zone_id = local.zone_ids[var.main_domain]
   name    = "mx.${var.main_domain}"
   type    = "A"
@@ -25,6 +29,7 @@ resource "aws_route53_record" "client_spf" {
   name     = each.value.domain
   type     = "TXT"
   ttl      = 300
+  # The value here (mail_server_ip) is known after apply, which is fine for the records attribute
   records  = ["v=spf1 mx ip4:${var.mail_server_ip} -all"]
 }
 
